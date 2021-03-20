20a5455e71170cb2756c679cfa177073a1af9012
= <=>
2021-03-20 15:02:03 -0500

    updated code

--- JavaMidterm-main/src/datastructure/UseQueue.java
+++ JavaMidterm-main/src/datastructure/UseQueue.java
@@ -1,7 +1,6 @@
 package datastructure;
 
-import java.util.PriorityQueue;
-import java.util.Random;
+import java.util.*;
 
 public class UseQueue {
 
@@ -12,13 +11,29 @@ public class UseQueue {
          *
          * Store and retrieve data from/to a database table.
          */
+
+        Queue<String> q = new LinkedList<>();
+        q.add("Anis");
+        q.add("Belhak");
+        System.out.println(q);
+        q.poll();
+
+        Iterator it = q.iterator() ;
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        System.out.println(q.remove("Anis"));
+        System.out.println(q);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
         PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
 
           Random rand = new Random();
           for (int i = 1; i < 27; i++) {
               queue.add(rand.nextInt(87));
         }
-
         System.out.println("Queue values are: " +queue);
         Integer head = queue.peek();
         System.out.println("Head of the queue is: " + head);
